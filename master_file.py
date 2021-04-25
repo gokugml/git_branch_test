2bf9831cfa40f3869c219dd2a17b528b52772ae6
Menglong Guan <guanmenglong@bytedance.com>
2021-04-25 23:45:59 +0800

    after_learn_2 on master before sync_first push 1

--- master_file.py
+++ master_file.py
@@ -2,7 +2,7 @@ def master():
     print('master_0')
     
 def feature_one():
-    print('feature_one_after_learn_1')
+    print('feature_one_after_learn_2')
     
 def feature_two():
     print('feature_two_after_learn_0')
