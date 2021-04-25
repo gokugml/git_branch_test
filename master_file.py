2458a62abd669af2263943d3496408c9756ea2fa
Menglong Guan <guanmenglong@bytedance.com>
2021-04-26 00:07:58 +0800

    without_f_push_1 push sync_first

--- master_file.py
+++ master_file.py
@@ -2,7 +2,7 @@ def master():
     print('master_0')
     
 def feature_one():
-    print('feature_one_after_learn_2')
+    print('feature_one_test_without_f_push_1 push sync_first')
     
 def feature_two():
     print('feature_two_after_learn_2 local additional')
