2626b7e0c8ca06aa128b15f436996bac15215c15
Menglong Guan <guanmenglong@bytedance.com>
2021-04-25 23:47:18 +0800

    after_learn_1 push to sync_first without rebase

--- master_file.py
+++ master_file.py
@@ -5,4 +5,4 @@ def feature_one():
     print('feature_one_after_learn_2')
     
 def feature_two():
-    print('feature_two_after_learn_0')
+    print('feature_two_after_learn_1 push to sync_first/rebase_with_masterAdditional without rebase')
